f9e05ef0dad4cd4edd89078794950fb9e37e002a
Shiba <subikuho@gmail.com>
2024-06-10 00:32:21 +0900

    fix-port

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ import (
 )
 
 func main() {
-	fmt.Println("listen and serve on :8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+			port = "8080"
+	}
+	fmt.Println("listen and serve on :" + port)
 	http.HandleFunc("/", hatebu.RenderHotentry)
-	err := http.ListenAndServe("127.0.0.1:8080", nil)
-
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Printf("error: %v", err)
 		os.Exit(1)
