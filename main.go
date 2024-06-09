ed89af95c98b8a8a7ee55816a41bf18c03cc1a5a
Shiba <subikuho@gmail.com>
2024-06-10 00:08:42 +0900

    to-8080

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 )
 
 func main() {
-	fmt.Println("listen and serve on :5000")
+	fmt.Println("listen and serve on :8080")
 	http.HandleFunc("/", hatebu.RenderHotentry)
-	err := http.ListenAndServe("127.0.0.1:5000", nil)
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
 
 	if err != nil {
 		fmt.Printf("error: %v", err)
